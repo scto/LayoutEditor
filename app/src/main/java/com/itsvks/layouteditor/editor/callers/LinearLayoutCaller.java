bdfea6f402c8d36546cb720f36597c5b24980489
Vivek <itsvks19@gmail.com>
2024-03-15 17:07:59 +0530

    fix: widgets within a LinearLayout are not laid out correctly (fixes #58)

--- app/src/main/java/com/itsvks/layouteditor/editor/callers/LinearLayoutCaller.java
+++ app/src/main/java/com/itsvks/layouteditor/editor/callers/LinearLayoutCaller.java
@@ -7,7 +7,6 @@ import android.widget.LinearLayout;
 import androidx.appcompat.widget.LinearLayoutCompat;
 
 import com.itsvks.layouteditor.utils.Constants;
-import com.itsvks.layouteditor.utils.DimensionUtil;
 
 import java.util.HashMap;
 
@@ -33,55 +32,5 @@ public class LinearLayoutCaller {
     for (String flag : flags) result |= Constants.gravityMap.get(flag);
     if (target instanceof LinearLayout) ((LinearLayout) target).setGravity(result);
     else ((LinearLayoutCompat) target).setGravity(result);
-  }
-
-  public static void setLayoutWeight(View target, String value, Context context) {
-    if (target instanceof LinearLayout)
-      ((LinearLayout.LayoutParams) target.getLayoutParams()).weight = Float.valueOf(value);
-    else ((LinearLayoutCompat.LayoutParams) target.getLayoutParams()).weight = Float.valueOf(value);
-    target.requestLayout();
-  }
-
-  public static void setLayoutMargin(View target, String value, Context context) {
-    int margin = (int) DimensionUtil.parse(value, context);
-    if (target instanceof LinearLayout)
-      ((LinearLayout.LayoutParams) target.getLayoutParams())
-          .setMargins(margin, margin, margin, margin);
-    else
-      ((LinearLayoutCompat.LayoutParams) target.getLayoutParams())
-          .setMargins(margin, margin, margin, margin);
-    target.requestLayout();
-  }
-
-  public static void setLayoutMarginLeft(View target, String value, Context context) {
-    int margin = (int) DimensionUtil.parse(value, context);
-    if (target instanceof LinearLayout)
-      ((LinearLayout.LayoutParams) target.getLayoutParams()).leftMargin = margin;
-    else ((LinearLayoutCompat.LayoutParams) target.getLayoutParams()).leftMargin = margin;
-    target.requestLayout();
-  }
-
-  public static void setLayoutMarginRight(View target, String value, Context context) {
-    int margin = (int) DimensionUtil.parse(value, context);
-    if (target instanceof LinearLayout)
-      ((LinearLayout.LayoutParams) target.getLayoutParams()).rightMargin = margin;
-    else ((LinearLayoutCompat.LayoutParams) target.getLayoutParams()).rightMargin = margin;
-    target.requestLayout();
-  }
-
-  public static void setLayoutMarginTop(View target, String value, Context context) {
-    int margin = (int) DimensionUtil.parse(value, context);
-    if (target instanceof LinearLayout)
-      ((LinearLayout.LayoutParams) target.getLayoutParams()).topMargin = margin;
-    else ((LinearLayoutCompat.LayoutParams) target.getLayoutParams()).topMargin = margin;
-    target.requestLayout();
-  }
-
-  public static void setLayoutMarginBottom(View target, String value, Context context) {
-    int margin = (int) DimensionUtil.parse(value, context);
-    if (target instanceof LinearLayout)
-      ((LinearLayout.LayoutParams) target.getLayoutParams()).bottomMargin = margin;
-    else ((LinearLayoutCompat.LayoutParams) target.getLayoutParams()).bottomMargin = margin;
-    target.requestLayout();
   }
 }
